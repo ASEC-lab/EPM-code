4cdd2e333a55c9f217a228b551804806dd539bce
Meir Goldenberg <meirgold@hotmail.com>
2023-11-12 10:09:28 +0000

    remove temporary code from main.py
    code moved to utils.py

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ from sympy.ntheory.modular import crt
 import time
 import argparse
 
+
 def epm_orig():
     """
     EPM implementation without encryption. Used for benchmarking.
@@ -70,30 +71,6 @@ def format_array_for_enc(arr: np.ndarray) -> np.ndarray:
     int_arr = rounded_arr.astype(int)
     return int_arr
 
-def estimate_max_val(ages, meth_vals):
-    flat_meth_vals = meth_vals.flatten().tolist()
-    ages_list = ages.tolist()
-    m = ages.size
-    n = meth_vals.flatten().size
-    age = 9999
-    iterations = 3
-    factor = 1
-    for i in range(iterations):
-        max_age = age
-        max_meth_val = 99
-        lambda_1 = (m * max_age)**2
-        r_i = factor * (m * (2 * m * max_age) * max_meth_val)
-        s_i = m * (max_age * m * max_age + m * (max_age**2)) * max_meth_val
-
-        age = n * r_i * (lambda_1 * max_meth_val + s_i)
-        temp = n * factor * 6 * (m**4) * (max_age**3) * (max_meth_val**2)
-
-        factor = m * (r_i ** 2)
-        print("iter :", i, "estimated age: ", age, "age len: ", len(str(age)))
-
-        print("alternate age: ", temp, "aget len: ", len(str(temp)))
-
-
 def epm_orig_new_method():
     max_iterations = 100
     rss = 0.0000001
@@ -112,14 +89,12 @@ def epm_orig_new_method():
     correlated_meth_val = train_methylation_values[correlated_meth_val_indices, :]
     #correlated_meth_val = train_methylation_values
 
-    # uncommet these lines to use the rounded integer values for this algorithm
+    # uncomment these lines to use the rounded integer values for this algorithm
     # unfortunately, the numbers are so large that we receive an overflow
     # but this is a good method to print out the expected number sizes we may reach
 
     formatted_ages = format_array_for_enc(train_ages)
     formatted_correlated_meth_val = format_array_for_enc(correlated_meth_val)
-
-    estimate_max_val(formatted_ages, formatted_correlated_meth_val)
 
     #formatted_ages = train_ages
     #formatted_correlated_meth_val = correlated_meth_val
@@ -128,83 +103,14 @@ def epm_orig_new_method():
     ages = epm.calc_model_new_method()
     return ages
 
+
 def test_do_multi_process(n, p, c, r, b):
     do = DO()
-    #ages = do.calc_model_multi_process(num_of_primes=30, enc_n=2**13, correlation=0.80)
     ages = do.calc_model_multi_process(num_of_primes=p, enc_n=2**n, correlation=c, rounds=r, prime_bits=b)
     return ages
 
 
-def test_arr_sum():
-    plaintext_prime = 1462436364289
-    csp = CSP(plaintext_prime, 2**13)
-    mle = MLE(csp)
-
-    arr = np.arange(1, 20)
-    enc_arr = csp.encrypt_array(arr)
-
-    tic = time.perf_counter()
-    new_sum = mle.calc_encrypted_array_sum(enc_arr, 3)
-
-
-    dec_arr = csp.decrypt_arr(new_sum)
-    print(dec_arr[0])
-
-    tic = time.perf_counter()
-    mle.calc_encrypted_array_sum(enc_arr, len(arr))
-    print("new sum method took: ", time.perf_counter()-tic, "seconds")
-
-
-def test_num_of_mult():
-    pyfhelCtxt = Pyfhel()
-    test_arr = np.array([2, 0, 0, 0])
-    mult_arr = np.array([4, 0, 0, 0])
-    square_arr = np.array([4, 0, 0, 0])
-    for poly in range(14, 15):
-        print("Generating context for poly:", poly, "\n")
-        pyfhelCtxt.contextGen("bfv", n=2**poly, t_bits=20, sec=128)
-        pyfhelCtxt.keyGen()
-        pyfhelCtxt.rotateKeyGen()
-        pyfhelCtxt.relinKeyGen()
-        test_arr_encoded = pyfhelCtxt.encodeInt(test_arr)
-        test_arr_encrypted = pyfhelCtxt.encryptPtxt(test_arr_encoded)
-        mult_arr_encoded = pyfhelCtxt.encodeInt(mult_arr)
-        mult_arr_encrypted = pyfhelCtxt.encryptPtxt(mult_arr_encoded)
-        square_arr_encoded = pyfhelCtxt.encodeInt(square_arr)
-        square_arr_encrypted = pyfhelCtxt.encryptPtxt(square_arr_encoded)
-        i = 0
-        square_arr_encrypted = square_arr_encrypted**2
-        square_arr_encrypted = ~square_arr_encrypted
-        noise_level = pyfhelCtxt.noise_level(test_arr_encrypted)
-        while (noise_level > 0) and (i < 12):
-            test_arr_encrypted = ~test_arr_encrypted
-            test_arr_encrypted = test_arr_encrypted * mult_arr_encrypted
-            noise_level = pyfhelCtxt.noise_level(test_arr_encrypted)
-            print("iteration", i, " noise level: ", noise_level, "\n")
-            i += 1
-        print("Noise level reached 0 after ", i, "iterations\n")
-
-        test_arr_encoded = pyfhelCtxt.encodeInt(test_arr)
-        test_arr_encrypted = pyfhelCtxt.encryptPtxt(test_arr_encoded)
-        i = 0
-        noise_level = pyfhelCtxt.noise_level(test_arr_encrypted)
-        while (noise_level > 0) and (i < 12):
-            test_arr_encrypted = ~test_arr_encrypted
-            if i < 2:
-                test_arr_encrypted = test_arr_encrypted * mult_arr_encrypted
-            else:
-                test_arr_encrypted = test_arr_encrypted * square_arr_encrypted
-            noise_level = pyfhelCtxt.noise_level(test_arr_encrypted)
-
-            print("iteration ", i, " noise level: ", noise_level, "\n")
-            i += 1
-        print("Noise level reached 0 after ", i, "iterations\n")
-
-
-
 def main(n, p, c, r, b):
-
-
     # this runs the encrypted version
     ages = test_do_multi_process(n, p, c, r, b)
     # epm cleartext testing using the new algorithm without division
@@ -240,7 +146,6 @@ if __name__ == '__main__':
 
     # for large dataset run:
     # n=14 p=52, c=0.8, r=3, b=30
-
 
     if args.polynomial:
         n = int(args.polynomial)
